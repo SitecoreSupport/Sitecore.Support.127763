4af867a37e19cb29a610884b623fec300ba659a9
Dmitry Kolinchuk <dmko@sitecore.net>
2017-10-16 16:21:52 +0300

    Original code

---
+++ src/Sitecore.Support.127763/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.127763")]
+[assembly: AssemblyProduct("Sitecore.Support.127763")]
+[assembly: ComVisible(false)]
